b207f698675a56d7f8be592a266fdb2f9fae94e5
Lucas Merchant <lbmerchant93@gmail.com>
2021-01-15 11:17:20 -0700

    Play with fetching data, get all travelers data to display

--- src/index.js
+++ src/index.js
@@ -7,4 +7,72 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// Global Variables
+let allDestinations;
+let allTravelers;
+let allTrips;
+
+// QuerySelectors
+let destinationsDOM = document.querySelector(".destinations");
+let travelersDOM = document.querySelector(".travelers");
+let tripsDOM = document.querySelector(".trips");
+
+// Eventlisteners
+window.addEventListener("load", gatherAPIInfo)
+
+function gatherAPIInfo() {
+  Promise.all([retrieveDestinations(), retrieveTravelers(), retrieveTrips()])
+  .then(data => {
+    allTravelers = data[1];
+
+    displayFetchedTravelers(allTravelers);
+  })
+}
+
+// Fetch Data
+function retrieveDestinations() {
+  return fetch("http://localhost:3001/api/v1/destinations")
+      .then(response => response.json())
+      .catch(err => {
+        alert("Sorry! We are having trouble getting the data, try again later!")
+      })
+}
+function retrieveTravelers() {
+  return fetch("http://localhost:3001/api/v1/travelers")
+      .then(response => response.json())
+      .catch(err => {
+        alert("Sorry! We are having trouble getting the data, try again later!")
+      })
+}
+function retrieveTrips() {
+  return fetch("http://localhost:3001/api/v1/trips")
+      .then(response => response.json())
+      .catch(err => {
+        alert("Sorry! We are having trouble getting the data, try again later!")
+      })
+}
+
+// Display all travelers
+function displayFetchedTravelers(travelersData) {
+  travelersData.travelers.forEach(traveler => {
+    travelersDOM.innerHTML +=
+    `<p>${traveler.id}, ${traveler.name}, ${traveler.travelerType} </p>`
+  })
+}
+
+// Display all trips
+
+// Display all destinations
+
+
+
+
+
+
+
+
+
+
+
+
+//
