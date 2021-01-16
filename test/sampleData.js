fd59a3f5a6a7ee24b2ebff9976be39728eb90099
Lucas Merchant <lbmerchant93@gmail.com>
2021-01-16 12:48:09 -0700

    Add test for addTrip method on Traveler, might be changed later after creating Trip class

--- test/sampleData.js
+++ test/sampleData.js
@@ -29,25 +29,25 @@ const sampleTravelerData = [{
 }];
 
 const sampleTripData = [{
-    "id": 1,
-    "userID": 44,
-    "destinationID": 49,
-    "travelers": 1,
-    "date": "2019/09/16",
-    "duration": 8,
+    "id": 117,
+    "userID": 1,
+    "destinationID": 28,
+    "travelers": 3,
+    "date": "2021/01/09",
+    "duration": 15,
     "status": "approved",
     "suggestedActivities": []
   },
   {
-    "id": 2,
-    "userID": 35,
-    "destinationID": 25,
-    "travelers": 5,
-    "date": "2020/10/04",
-    "duration": 18,
-    "status": "pending",
-    "suggestedActivities": []
-  }, {
+   "id": 166,
+   "userID": 2,
+   "destinationID": 7,
+   "travelers": 2,
+   "date": "2020/03/05",
+   "duration": 6,
+   "status": "approved",
+   "suggestedActivities": []
+ }, {
     "id": 3,
     "userID": 3,
     "destinationID": 22,
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -4,13 +4,16 @@ const expect = chai.expect;
 import Traveler from '../src/traveler';
 import sampleData from '../test/sampleData.js'
 const sampleTravelerData = sampleData.sampleTravelerData;
+const sampleTripData = sampleData.sampleTripData;
 
 describe('See if the tests are running', () => {
-  let traveler1, traveler2;
+  let traveler1, traveler2, trip1, trip2;
 
   beforeEach(() => {
     traveler1 = new Traveler(sampleTravelerData[0]);
     traveler1 = new Traveler(sampleTravelerData[1]);
+    trip1 = sampleTripData[0];
+    trip2 = sampleTripData[1];
   });
 
   it('should be a function', () => {
@@ -34,5 +37,31 @@ describe('See if the tests are running', () => {
       expect(traveler2.upcomingTrips).to.deep.equal([]);
       expect(traveler2.pendingTrips).to.deep.equal([]);
       expect(traveler2.pastTrips).to.deep.equal([]);
+    })
+
+    it('shoud have a method to add to the correct array given the array\'s property name and trip object', () => {
+      traveler1.addTrip('currentTrips', trip1);
+      expect(traveler1.currentTrips).to.deep.equal([{
+          "id": 117,
+          "userID": 1,
+          "destinationID": 28,
+          "travelers": 3,
+          "date": "2021/01/09",
+          "duration": 15,
+          "status": "approved",
+          "suggestedActivities": []
+        }]);
+
+        traveler2.addTrip('pastTrips', trip2);
+        expect(traveler1.currentTrips).to.deep.equal([{
+         "id": 166,
+         "userID": 2,
+         "destinationID": 7,
+         "travelers": 2,
+         "date": "2020/03/05",
+         "duration": 6,
+         "status": "approved",
+         "suggestedActivities": []
+       }]);
     })
 });
