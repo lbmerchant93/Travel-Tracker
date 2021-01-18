d97ec455d30a9db1792ee3fabc7e3f30ae952d11
Lucas Merchant <lbmerchant93@gmail.com>
2021-01-17 18:04:16 -0700

    Add borders to trips, display flex to card containers

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -90,12 +90,6 @@ let domUpdates = {
       })
     }
   },
-  //  basic layout for cards
-  //  <div class="current-trip card">
-  //    <p>this is a place holder for the image</p>
-  //    <h4 class="trip-destination">trip destination based on destination id</h4>
-  //   <p class="details">start date && return date && number of travelers</p>
-  // </div>
 
 }
 
